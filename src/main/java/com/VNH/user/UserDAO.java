4cf3116f6c8f9523e184e7d51a2cad11742b99d1
fpt-theluis <logan25092001@gmail.com>
2023-06-16 02:07:33 +0700

    first commit

---
+++ src/main/java/com/VNH/user/UserDAO.java
@@ -0,0 +1,14 @@
+package com.VNH.user;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author NguyenTheAnh
+ */
+public class UserDAO {
+    
+}
---
+++ src/main/java/com/VNH/utils/DBUtils.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.VNH.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author NguyenTheAnh
+ */
+public class DBUtils {
+
+    public static Connection makeConnection() throws ClassNotFoundException, SQLException {
+        Connection conn = null;
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String URL = "jdbc:sqlserver://localhost:1433;databaseName=VNH";
+        conn = DriverManager.getConnection(URL, "sa", "12345");
+        return conn;
+    }
+    
+    public static void main(String[] args) {
+        try{
+            System.out.println(DBUtils.makeConnection());
+        }catch(ClassNotFoundException | SQLException e){}
+    }
+}
